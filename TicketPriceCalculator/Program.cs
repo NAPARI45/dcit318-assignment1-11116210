44c6842663c0cb10f9144d9ea30ff3311c37e1de
NAPARI45 <hnhamza@st.ug.edu.gh>
2025-08-03 19:08:19 +0000

    Added Ticket Price Calculator application

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                if (age <= 12 || age >= 65)
+                    Console.WriteLine("Your ticket price is: GHC7");
+                else
+                    Console.WriteLine("Your ticket price is: GHC10");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid age.");
+            }
+        }
+    }
+}
